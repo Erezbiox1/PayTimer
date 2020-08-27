8079c109101d7ddeb1c3edac2916a8bcf99dd740
Erez Rotem <Erezbiox1@gmail.com>
2020-08-27 19:23:33 +0300

    Added a shift options menu

--- app/src/main/java/com/erezbiox1/paytimer/ListShifts/ShiftsAdapter.java
+++ app/src/main/java/com/erezbiox1/paytimer/ListShifts/ShiftsAdapter.java
@@ -5,10 +5,16 @@
 package com.erezbiox1.paytimer.ListShifts;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +22,7 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.erezbiox1.paytimer.EditShift.EditShiftActivity;
 import com.erezbiox1.paytimer.R;
 import com.erezbiox1.paytimer.Room.Shift;
 
@@ -66,10 +73,12 @@ public class ShiftsAdapter extends RecyclerView.Adapter<ShiftsAdapter.ViewHolder
         notifyDataSetChanged();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
 
+        private Shift shift;
         private CardView cardView;
         private TextView dayOfTheWeek, fromHour, toHour, date, totalPayout, totalHours;
+        private ImageView optionsButton;
 
         ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -82,9 +91,13 @@ public class ShiftsAdapter extends RecyclerView.Adapter<ShiftsAdapter.ViewHolder
             date = itemView.findViewById(R.id.shift_date);
             totalPayout = itemView.findViewById(R.id.shift_total_payout);
             totalHours = itemView.findViewById(R.id.shift_total_hours);
+            optionsButton = itemView.findViewById(R.id.shift_options_button);
+
+            optionsButton.setOnClickListener(this);
         }
 
         private void setShift(Shift shift){
+            this.shift = shift;
 
             long startDate = shift.getStartTime();
             long endDate = shift.getEndTime();
@@ -103,6 +116,38 @@ public class ShiftsAdapter extends RecyclerView.Adapter<ShiftsAdapter.ViewHolder
 
         private void set(TextView textView, String pattern, long time){
             textView.setText(new SimpleDateFormat(pattern, Locale.getDefault()).format(new Date(time)));
+        }
+
+        @Override
+        public void onClick(View view) {
+            PopupMenu popupMenu = new PopupMenu(context, view);
+            MenuInflater inflater = popupMenu.getMenuInflater();
+            inflater.inflate(R.menu.shift_options_menu, popupMenu.getMenu());
+            popupMenu.setOnMenuItemClickListener(this);
+            popupMenu.show();
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()){
+                case R.id.shift_options_edit:
+                    clickEditOption();
+                    break;
+                case R.id.shift_options_delete:
+                    Toast.makeText(context, "NOT AN OPTION", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return false;
+        }
+
+        private void clickEditOption(){
+            Intent editShiftIntent = new Intent(context, EditShiftActivity.class);
+
+            editShiftIntent.putExtra(EditShiftActivity.SHIFT_ID_EXTRA, shift.getId());
+            editShiftIntent.putExtra(EditShiftActivity.START_TIME_EXTRA, shift.getStartTime());
+            editShiftIntent.putExtra(EditShiftActivity.END_TIME_EXTRA, shift.getEndTime());
+
+            context.startActivity(editShiftIntent);
         }
     }
 }
